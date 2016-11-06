f7e7f7f19c595ac9ddcdb76080c30a8a20d2db00
Abdul Wahab <cs.abdulwahab@gmail.com>
2016-11-06 20:03:28 +0500

    Version Control Tab  add and then add to git

--- app/src/main/java/com/example/abdulwahab/flexiblelayoutusingfragments/adapters/EmployeeAdapter.java
+++ app/src/main/java/com/example/abdulwahab/flexiblelayoutusingfragments/adapters/EmployeeAdapter.java
@@ -2,10 +2,13 @@ package com.example.abdulwahab.flexiblelayoutusingfragments.adapters;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
+import com.example.abdulwahab.flexiblelayoutusingfragments.R;
 import com.example.abdulwahab.flexiblelayoutusingfragments.model.Employee;
 
 import java.util.List;
@@ -15,14 +18,26 @@ import java.util.List;
  */
 public class EmployeeAdapter extends ArrayAdapter<Employee> {
     public EmployeeAdapter(Context context, List<Employee> objects) {
-        super(context, android.R.layout.simple_list_item_1, objects);
+        super(context, 0, objects);
     }
 
     @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        View view = super.getView(position, convertView, parent);
 
+        View view = convertView;
+        if (convertView == null)
+            view = LayoutInflater.from(getContext()).inflate(R.layout.row_custom_list, parent, false);
+
+
+        // ViewHolder Design Pattern Not Implemented here
+
+        Employee emp = getItem(position);
+        TextView txtEmpID = (TextView) view.findViewById(R.id.txtEmpID);
+        TextView txtEmpName = (TextView) view.findViewById(R.id.txtEmpName);
+
+        txtEmpID.setText(""+emp.id);
+        txtEmpID.setText(""+emp.getName());
 
         return view;
     }
